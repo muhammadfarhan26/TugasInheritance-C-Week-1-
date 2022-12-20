ed345c484edbdaf7db80602d7da98a0471b60e71
muhammadfarhan26 <45418738+muhammadfarhan26@users.noreply.github.com>
2022-12-20 11:35:53 +0700

    tes

---
+++ Nomor1/CEO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasInheritance.Nomor1
+{
+    internal class CEO
+    {
+        /*Id - int
+Name - string
+BirthDate - string format dd-M-yyyy
+Address - string
+Phone - string
+Position - string
+Lists (1 atau lebih seusaikan dengan kebutuhan)*/
+
+        int? id;
+        string? name;
+        string? birthdate;
+        string? address;
+        string? phone;
+        string? position;
+
+        public CEO()
+        {
+            position = "CEO"; // Set the initial value for model
+        }
+
+
+    }
+}
---
+++ Nomor1/ManagerSales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasInheritance.Nomor1
+{
+    internal class ManagerSales : CEO
+    {
+        int id;
+        string name;
+        string birthdate;
+        string address;
+        string phone;
+        string position;
+
+        public ManagerIT()
+        {
+            position = "Manager Sales"; // Set the initial value for model
+        }
+}
